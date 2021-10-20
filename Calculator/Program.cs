3378b15690997ed75d8c32cae33dcec22f300d12
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:50:15 +0300

    Week8-Functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number:");
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Valitehe ('+', '-', '*', '/'");
+            char userOperation = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperation)
+            {
+                case '+':
+                    AddTwoNumbers(num1, secondnumber);
+                    break;
+                case '-':
+                    SubtractTwoNumbers(num1, secondnumber);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(num1, secondnumber);
+                    break;
+                case '/':
+                    DivideTwoNumbers(num1, secondnumber);
+                    break;
+                default:
+                    Console.WriteLine("Vale tehe");
+                    break;
+            }
+            
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+
+        private static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+        }
+
+        private static void SubtractTwoNumbers(int x, double y)
+        {
+            double result = x - y;
+            Console.WriteLine($"{x} - {y} = {result}");
+        }
+
+        private static void MultiplyTwoNumbers(int x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine($"{x} * {y} = {result}");
+        }
+
+    }
+}
